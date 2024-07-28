58e075df8a4ecb745af054a4088ac2ce84b4e513
bright_spark <markpassey@btinternet.com>
2024-07-28 09:09:52 +0100

    Added purchase chat message

--- src/main/kotlin/brightspark/brightereconomy/screen/ShopCustomerScreenHandler.kt
+++ src/main/kotlin/brightspark/brightereconomy/screen/ShopCustomerScreenHandler.kt
@@ -6,11 +6,15 @@ import brightspark.brightereconomy.economy.EconomyState
 import brightspark.brightereconomy.economy.PlayerAccount
 import brightspark.brightereconomy.economy.TransactionExchangeResult
 import brightspark.brightereconomy.network.CustomerScreenPurchasePacket
+import brightspark.brightereconomy.util.Util
 import brightspark.brightereconomy.util.getSpaceFor
 import brightspark.brightereconomy.util.property
 import io.wispforest.owo.client.screens.SyncedProperty
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.item.ItemStack
+import net.minecraft.text.MutableText
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import kotlin.math.min
 
 class ShopCustomerScreenHandler(
@@ -38,10 +42,24 @@ class ShopCustomerScreenHandler(
 				.log()
 
 			val player = playerInventory.player
+			val itemAmount = forSaleStack.get().count * packet.amount
+			val cost = cost.get().toLong() * packet.amount
 			val result = EconomyState.get()
-				.exchange(playerAccount.get().uuid, ownerUuid, cost.get().toLong() * packet.amount, player.entityName)
+				.exchange(playerAccount.get().uuid, ownerUuid, cost, player.entityName)
 
 			if (result == TransactionExchangeResult.SUCCESS) {
+				fun whiteText(): MutableText = Text.empty().styled { it.withFormatting(Formatting.WHITE) }
+				fun whiteText(text: String): Text = Text.literal(text).styled { it.withFormatting(Formatting.WHITE) }
+				player.sendMessage(
+					Text.translatable(
+						"screen.brightereconomy.player_shop.purchase.success",
+						whiteText(itemAmount.toString()),
+						whiteText().append(forSaleStack.get().name),
+						whiteText(Util.formatMoney(cost)),
+						whiteText(Util.getUsername(ownerUuid) ?: "<unknown>")
+					).styled { it.withFormatting(Formatting.GRAY, Formatting.ITALIC) }
+				)
+
 				var amountToGive = packet.amount
 				val maxStackSize = forSaleStack.get().maxCount
 				while (amountToGive > 0) {
